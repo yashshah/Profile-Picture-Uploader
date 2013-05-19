01c1696c1f0aaec23c0eb5eea1e86049aeeab0d7
Yash Shah <mail@yashshah.com>
2013-05-19 16:44:12 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageUploader::Application.config.secret_token = '8be33c01df59f0fc83555f415422a8f0294014fd6e4fc0db81fbdd7fe0d7f9e225a58d123a8946f7f45817f9be0b3dda94c238f17b10890503f0a076e00bd082'
