1a0ffb5f2645be4887748bc13d0aa1c49fb7afba
Yash Shah <mail@yashshah.com>
2013-05-19 17:12:39 +0530

    Edit migration for users

--- db/migrate/20130519113713_create_users.rb
+++ db/migrate/20130519113713_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+      t.has_attached_file :profile_picture
 
       t.timestamps
     end
