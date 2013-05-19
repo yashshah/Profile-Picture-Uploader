6c8f74bac22772996f058be2ec806dbb73e72f1b
Yash Shah <mail@yashshah.com>
2013-05-19 17:13:29 +0530

    Add root to (index page) in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ImageUploader::Application.routes.draw do
   resources :users
-
+  root to: 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
