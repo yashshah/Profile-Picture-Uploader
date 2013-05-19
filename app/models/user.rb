569477c45b3a1e0365f9529a337de1c274b380e1
Yash Shah <mail@yashshah.com>
2013-05-19 17:37:56 +0530

    Add accessible attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+	
+	attr_accessible :name, :profile_picture
+	validates_presence_of :name
+  
+    has_attached_file :profile_picture, :styles => {:thumb  => "50x50#",
+		                                            :small  => "150x150>",
+		                                            :medium => "300x300>",
+		                                            :large  => "400x400>" }
 end
